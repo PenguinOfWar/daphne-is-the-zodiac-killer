33eaf8cb538040182a4796f0e5257023f3b82d00
Darryl Walker <darryljwalker@gmail.com>
2020-02-21 17:34:05 +0000

    Initial commit with customisations on top of latest CRA

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Provider } from 'react-redux';
+import { Router, Route } from 'react-router-dom';
+import { createBrowserHistory as createHistory } from 'history';
+import configureStore from './stores/store';
+
+import App from './containers/App/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './sass/index.scss';
+
+const history = createHistory();
+
+const initialState = {};
+
+const AppLoader = (
+  <Provider store={configureStore({ initialState, history })}>
+    <Router history={history}>
+      <Route path="*" component={props => <App {...props} />} />
+    </Router>
+  </Provider>
+);
+
+ReactDOM.render(AppLoader, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
